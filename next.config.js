c122c8a542f3187d4ee88619cae825beb1b48187
Joshua L <joshua.leong@singaporetech.edu.sg>
2024-07-17 15:25:00 +0800

    Updated config, added image sharp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
+import('next').NextConfig
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ['react-hotjar'],
+  images: {
+    // domains: ["plus.unsplash.com"], // deprecated
+    remotePatterns: [ // docs: https://nextjs.org/docs/pages/api-reference/components/image-legacy#remote-patterns
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  }
 }
 
 module.exports = nextConfig
